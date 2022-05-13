97f6ba760420df48642ef28182607955411dc859
ryann127 <ryann.goldberg127@gmail.com>
2022-05-12 19:52:42 -0600

    added self reference between manager id and employee id in employee table

--- db/schema.sql
+++ db/schema.sql
@@ -18,15 +18,19 @@ CREATE TABLE empRole (
     REFERENCES department(id),
     ON DELETE SET NULL,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE employee (
     id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
     FOREIGN KEY (role_id),
     REFERENCES empRole(id),
+    ON DELETE SET NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id),
     ON DELETE SET NULL
-)
+);

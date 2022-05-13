6cc1cf8a4f386ececa8868fe30ad703e4c167a5b
ryann127 <ryann.goldberg127@gmail.com>
2022-05-12 19:48:54 -0600

    added three initial tables for department, role, and employee

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE empRole (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    dept_id INT,
+    FOREIGN KEY(dept_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+    FOREIGN KEY (role_id),
+    REFERENCES empRole(id),
+    ON DELETE SET NULL
+)

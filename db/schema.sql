8a7fc2c08cfbda54abe7ab196c454785fc054152
ryann127 <ryann.goldberg127@gmail.com>
2022-05-14 13:05:45 -0600

    debugging my schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,33 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT,
-  dept_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE empRole (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     dept_id INT,
-    FOREIGN KEY(dept_id),
-    REFERENCES department(id),
-    ON DELETE SET NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY(dept_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id),
-    REFERENCES empRole(id),
-    ON DELETE SET NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (manager_id),
-    REFERENCES employee(id),
-    ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES empRole(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
